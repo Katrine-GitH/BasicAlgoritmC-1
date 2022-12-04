f4c339995473b3ead4335560a297a0a60afb0776
Kate <tsvetkova.e.i@mail.ru>
2022-12-04 23:15:34 +0300

    11

--- Dayoftheweek.cs
+++ Dayoftheweek.cs
@@ -7,13 +7,19 @@
 //1 -> нет
 Console.WriteLine("Напишите число дня недели");
 int a = int.Parse(Console.ReadLine());
-int a=1;
-int b=2;
-int c=3;
-int d=4;
-int e=5;
-int f=6;
-int g=7;
-if (a ==6) or (a==7) {
+if (a < 6)
+{
+    Console.WriteLine("no");
+}
+if (a == 7)
+{
     Console.WriteLine("yes");
+}
+if (a == 6)
+{
+    Console.WriteLine("yes");
+}
+if (a > 7)
+{
+    Console.WriteLine("no");
 }
